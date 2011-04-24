cd7e6c950d7056d454a6ab5ab104c364a4070ca3
Ricardo Sampayo <ricardo9588@gmail.com>
2011-04-24 14:20:08 -0430

    validación en orden => index (No se pueden enviar paquetes ya enviados)

--- courier/app/helpers/ordens_helper.rb
+++ courier/app/helpers/ordens_helper.rb
@@ -1,2 +1,11 @@
 module OrdensHelper
+	
+	def validarOrden (id)
+		@historico = Historico.where(:ordens_id => id , :tipo => 'inicio').first
+		if @historico.nil?
+			@historico = link_to 'Enviar', enviar_path(id)
+		else
+			@historico = 'Enviado'
+		end	
+	end
 end
