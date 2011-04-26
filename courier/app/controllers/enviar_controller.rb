54d95b5dbf37b00dfe51f8f278b028b66509e83b
Ricardo Sampayo <ricardo9588@gmail.com>
2011-04-25 21:30:50 -0430

    precios mas caros jejejejej

--- courier/app/controllers/enviar_controller.rb
+++ courier/app/controllers/enviar_controller.rb
@@ -52,7 +52,7 @@ class EnviarController < ApplicationController
 		@direccion1= Direccion.find_by_sql("select * from historicos h, direccions d where d.id=h.direccions_id and h.tipo='inicio' and h.ordens_id=" + id).first
 		@direccion2= Direccion.find_by_sql("select * from historicos h, direccions d where d.id=h.direccions_id and h.tipo='fin' and h.ordens_id=" + id).first
 		@distancia=(Math.sqrt((@direccion1.lat-@direccion2.lat)**2 + (@direccion1.lng-@direccion2.lng)**2))*10
-		@monto = (60*@contador) + (( @peso + @distancia )* @contador) *10
+		@monto = ((60*@contador) + (( @peso + @distancia )* @contador) *10)+50
 		@lala=@monto
 	end
 
