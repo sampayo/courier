89ec68868e00a2d675a8f2c91b448c00f69b3bd1
albertogg <albertogrespan@gmail.com>
2011-05-11 21:31:14 -0430

    Seeds.

--- courier/db/seeds.rb
+++ courier/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+# Companium.new([{:id => 1}, {:nombre => 'compania1'}, {:rif => 1234567},
+  # {:direccionF => 'aqui esta'}, {:telefono => '5555444'}])
+Companium.create! :id => 1,
+                  :nombre => 'compania1',
+                  :rif => 1234567,
+                  :direccionF => 'aqui esta',
+                  :telefono => '5555444',
+                  :created_at => '2011-04-26 02:39:12',
+                  :updated_at => '2012-04-26 02:39:12'
