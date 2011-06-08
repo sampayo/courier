468d090cd2c8871d7a584095f8096cfd4caae5b2
albertogg <albertogrespan@gmail.com>
2011-06-07 19:59:17 -0430

    internacionalizacion, css

--- courier/app/helpers/ordens_helper.rb
+++ courier/app/helpers/ordens_helper.rb
@@ -3,7 +3,7 @@ module OrdensHelper
   def validarOrden (id)
     @historico = Historico.where(:ordens_id => id , :tipo => 'inicio').first
     if @historico.nil?
-      @historico = link_to 'Enviar', enviar_path(id)
+      @historico = link_to t('enviar'), enviar_path(id)
     else
       @historico = 'Enviado'
     end
@@ -12,9 +12,9 @@ module OrdensHelper
   def retorno(id)
     @persona = Persona.find(id)
     if @persona.empleados_id.nil?
-      link_to 'Back', ordens_path
+      link_to t('regresar'), ordens_path
     else
-      link_to 'Back', despachador_path
+      link_to t('regresar'), despachador_path
     end
   end
 
